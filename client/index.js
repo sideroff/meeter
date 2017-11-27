32022c044aea73834066afc14e90730c7778d946
Ivan Siderov <ivan.siderov@quenti.tech>
2017-11-27 20:00:16 +0200

    adds the root App component

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
-window.onload = function () {
-    console.log('hey')
-    console.log('boi')
-}
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+ReactDom.reander(App,document.getElementById('app'))
